9cd3bb3c5f8e32398d2727f109d7aa7d033c9153
Andy Moore <moorejandy@gmail.com>
2020-02-29 18:17:22 -0500

    Updates

---
+++ precache-manifest.14298d2723c11f5aa2b99f1d197cbf34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be73d8734b03cd929102b7c9d5945a43",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "01f18a8d75d73b3fb1d6",
+    "url": "/clicky-game/static/css/main.5b3f67b1.chunk.css"
+  },
+  {
+    "revision": "e16b671658e369eb3ee8",
+    "url": "/clicky-game/static/js/2.bb68de2a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/clicky-game/static/js/2.bb68de2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f18a8d75d73b3fb1d6",
+    "url": "/clicky-game/static/js/main.3a7c2dc6.chunk.js"
+  },
+  {
+    "revision": "42c2b29e455c8c344d7d",
+    "url": "/clicky-game/static/js/runtime-main.622bcec2.js"
+  }
+]);
